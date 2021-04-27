2d39d9bef3862da5323f68394257553cf7d1dd95
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-27 22:11:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Add your functions here
+<<<<<<< HEAD
 function map(num, func){
   return num.map(func);
 }
@@ -7,10 +8,17 @@ function reduce(num,func){
   return num.reduce(func);
 }
 
+=======
+function number(num, func){
+  return num.map(func);
+}
+
+>>>>>>> cea81bc26bedf16cee26a5ac9f4e4bbccafd41e8
 function mapToSquare(x){
   return x * x;
 }
 
+<<<<<<< HEAD
 function reduce(a, c, startingValue) {
   let total;
   if (startingValue) {
@@ -29,3 +37,6 @@ function reduce(a, c, startingValue) {
 }
 
 map(num,double);
+=======
+return number(num,double);
+>>>>>>> cea81bc26bedf16cee26a5ac9f4e4bbccafd41e8
